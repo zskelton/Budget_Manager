66b8741d127fbb7b18a70d8d23638b1e98ad5c16
Zachary D. Skelton <zbert@hotmail.com>
2022-07-09 15:33:46 -0500

    Works: React and Electron on one command.

--- src/electron-starter.js
+++ src/electron-starter.js
@@ -1,11 +1,19 @@
 const {app, BrowserWindow} = require('electron')
-// const path = require('path')
+const url = require('url')
+const path = require('path')
 
 // Keep a global reference of the window object
 let mainWindow
 function createWindow () {
   mainWindow = new BrowserWindow({width: 800, height: 600})
-  mainWindow.loadURL('http://localhost:3000/')
+  // mainWindow.loadURL('http://localhost:3000/')
+  const startUrl = process.env.ELECTRON_START_URL
+    || url.format({
+      pathname: path.join(__dirname, '/../build/index.html'),
+      protocol: 'file:',
+      slashes: true
+    })
+  mainWindow.loadURL(startUrl)
   // mainWindow.webContents.openDevTools()
   mainWindow.on('closed', () => {
     mainWindow = null
